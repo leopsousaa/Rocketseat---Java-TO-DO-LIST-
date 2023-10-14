a2538c7be7a0fc0eaf71cfc2dac103b77552af4f
lesousa <lesousa@brasilseg.com.br>
2023-10-14 08:46:46 -0300

    feat/add multiple return

--- src/main/java/br/com/leosousa/todolist/controller/user/UserController.java
+++ src/main/java/br/com/leosousa/todolist/controller/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.leosousa.todolist.controller.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+
+    var user = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (user != null) {
+      System.out.println("Já existe esse usuário!");
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+    }
+
+    this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body("Usuário criado com sucesso!");
   }
 
 }
